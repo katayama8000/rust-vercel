bfaf8575856899651727c7030ea4439c5a0a584c
katayama <tattu.0310@gmail.com>
2021-12-31 15:31:58 +0900

    'generics'

--- generics.js
+++ generics.js
@@ -15,3 +15,23 @@ function copy(value) {
     return value;
 }
 copy({ name: "hello" });
+class DataBase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringDataBase = new DataBase();
+stringDataBase.add("apple");
+stringDataBase.add('banana');
+stringDataBase.add('grape');
+stringDataBase.remove('banana');
+console.log(stringDataBase.get());
--- generics.ts
+++ generics.ts
@@ -21,3 +21,24 @@ function copy<T extends { name: string }>(value: T): T {
 copy({ name: "hello" });
 
 type K = keyof { name: string; age: number };
+
+class DataBase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+
+const stringDataBase = new DataBase<string>();
+stringDataBase.add("apple");
+stringDataBase.add('banana')
+stringDataBase.add('grape')
+stringDataBase.remove('banana')
+
+console.log(stringDataBase.get())
