a7395aeed303ed00a784fff39b63fb16ad368f3b
katayama <tattu.0310@gmail.com>
2022-10-14 21:35:43 +0900

    feat/bind

---
+++ practice/bind.ts
@@ -0,0 +1,36 @@
+const myButton = {
+  content: "OK",
+  click() {
+    console.log(this);
+    console.log(this.content + " clicked");
+  },
+  yourButton: {
+    content: "No",
+    click() {
+      console.log(this);
+      console.log(this.content + " clicked your button");
+    },
+  },
+};
+
+const yourButton = {
+  content: "No",
+};
+
+const looseClick = myButton.click.bind(yourButton);
+//myButton.yourButton.click();
+looseClick();
+
+console.log("---------------------");
+
+const greeting = function (this: any) {
+  console.log(this);
+  console.log(this.name);
+  console.log(this.age);
+};
+//greeting();
+const object = { name: "John", age: 30 };
+const new_greeting = greeting.bind(object);
+new_greeting();
+
+console.log("---------------------");
