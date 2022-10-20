b842f5427e0c24494559eba457db8074a91119d8
katayama <tattu.0310@gmail.com>
2022-10-20 12:57:11 +0900

    update

--- sample/sample1.ts
+++ sample/sample1.ts
@@ -4,8 +4,18 @@ const friends = [
   { name: "Chris", age: 25, country: "Canada" },
 ] as const;
 
+// 値のユニオン型
 type FriendName = typeof friends[number]["name"];
 type FriendAge = typeof friends[number]["age"];
 type FriendCountry = typeof friends[number]["country"];
 // オブジェクトのユニオン型
 type Friend = typeof friends[number];
+
+const NAME = {
+  HOGE: "hoge",
+  FUGA: "fuga",
+} as const;
+type type1 = keyof typeof NAME;
+// => "HOGE" | "FUGA"
+type type2 = typeof NAME[keyof typeof NAME];
+// => "hoge" | "fuga"
