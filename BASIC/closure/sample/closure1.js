92f314870cedaa67b970c8d4d6597831c5f9f81e
katayama <tattu.0310@gmail.com>
2022-11-19 17:44:14 +0900

    update

--- BASIC/closure/sample/closure1.js
+++ BASIC/closure/sample/closure1.js
@@ -22,3 +22,21 @@ counter();
 counter();
 
 //クロージャー：特定の変数を特定の関数にしか使わせないためのもの
+
+const PrivateInfo = () => {
+    let importantNumber = 20001212
+    return {
+        getImportantNumber() {
+            return importantNumber;
+        },
+
+        setImportantNumber(number) {
+            importantNumber = number;
+        }
+    }
+}
+
+const privateInfo = PrivateInfo();
+console.log(PrivateInfo.getImportantNumber());
+privateInfo.setImportantNumber(20201230);
+console.log(privateInfo.getImportantNumber());
