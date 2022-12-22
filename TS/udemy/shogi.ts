2cd0c4d4befeb26c5c6c101a9493228542ebad4a
katayama8000 <tattu.0310@gmail.com>
2022-12-22 17:59:37 +0900

    Revert "fix: error"
    This reverts commit 1f8d24282aad526a92d7363e9fbcd1308493483a.

--- TS/udemy/shogi.ts
+++ TS/udemy/shogi.ts
@@ -1,6 +1,6 @@
-type Player = 'first' | 'second';
-type Suji = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type Dan = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+type Player = 'first' | 'second'
+type Suji = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type Dan = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 
 class Position {
   // private修飾子のついたプロパティは、Positionクラスから生まれたインスタンスのみで使用できる
@@ -12,12 +12,12 @@ class Position {
       return {
         suji: Math.abs(position.suji - this.suji),
         dan: Math.abs(Number(position.dan) - Number(this.dan)),
-      };
+      }
     } else {
       return {
         suji: Math.abs(position.suji - this.suji),
         dan: -Math.abs(Number(position.dan) - Number(this.dan)), // 段(縦の距離)は正負反転
-      };
+      }
     }
   }
 }
@@ -25,55 +25,55 @@ class Position {
 // Pieceは抽象クラス
 abstract class Piece {
   // protected修飾子のついたプロパティは、Pieceクラスとサブクラスから生まれたインスタンスで使用できる
-  protected position: Position;
+  protected position: Position
 
   constructor(private readonly player: Player, suji: Suji, dan: Dan) {
-    this.position = new Position(suji, dan);
+    this.position = new Position(suji, dan)
   }
 
   // パラメーターに渡された位置へ駒を移動するメソッド
   // publicなので、サブクラスでオーバーライド（上書き）できる
   moveTo(position: Position) {
-    this.position = position;
+    this.position = position
   }
 
   // 移動できるかどうか判定するメソッド
   // abstractをつけて宣言しておき、サブクラスで具体的に実装する
-  abstract canMoveTo(position: Position, player: Player): boolean;
+  abstract canMoveTo(position: Position, player: Player): boolean
 }
 
 // Pieceクラスを継承したOshoクラスを宣言
 class Osho extends Piece {
   canMoveTo(position: Position, player: Player): boolean {
     // 移動先に指定された位置(position)と現在の位置(this.position)を比較
-    const distance = this.position.distanceFrom(position, player);
+    const distance = this.position.distanceFrom(position, player)
     // 移動先との距離が2未満、つまり1マス以内なら移動できる
-    return distance.suji < 2 && distance.dan < 2;
+    return distance.suji < 2 && distance.dan < 2
   }
 }
 
 class Fu extends Piece {
   canMoveTo(position: Position, player: Player): boolean {
-    const distance = this.position.distanceFrom(position, player);
+    const distance = this.position.distanceFrom(position, player)
     // 移動先との距離が前方1マスであれば
-    return distance.suji === 0 && distance.dan === 1;
+    return distance.suji === 0 && distance.dan === 1
   }
 }
 
 class NariKin extends Fu {
   canMoveTo(position: Position, player: Player): boolean {
-    const distance = this.position.distanceFrom(position, player);
+    const distance = this.position.distanceFrom(position, player)
     return (
       distance.suji < 2 &&
       distance.dan < 2 && // 移動先が1マス以内
       distance.suji !== 0 &&
       distance.dan === -1 // 左後方と右後方には進めない
-    );
+    )
   }
 }
 
 class Game {
-  private pieces = Game.makePieces();
+  private pieces = Game.makePieces()
   private static makePieces() {
     return [
       new Osho('first', 5, '1'),
@@ -100,8 +100,8 @@ class Game {
       new Fu('second', 7, '7'),
       new Fu('second', 8, '7'),
       new Fu('second', 9, '7'),
-    ];
+    ]
   }
 }
 
-new Game();
+new Game()
