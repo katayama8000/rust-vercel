c94fe284d8b6392d20a5a5c29b698013a7384127
katayama8000 <tattu.0310@gmail.com>
2022-12-24 19:40:28 +0900

    update

--- Function/index1.js
+++ Function/index1.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const countList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // 代入
 let sum3 = 0;
@@ -10,3 +19,27 @@ let sum2 = countList.reduce((accumulator, currentValue) => {
     return accumulator + currentValue;
 }, 0);
 console.log(sum3 === sum2);
+function main1() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('hello');
+                resolve('OK');
+            }, 1000);
+        });
+        console.log('world');
+    });
+}
+main1();
+function main2() {
+    new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('hello');
+            resolve('OK');
+        }, 2000);
+    }).then((message) => {
+        console.log('world');
+        console.log(message);
+    });
+}
+main2();
--- Function/index1.ts
+++ Function/index1.ts
@@ -12,3 +12,30 @@ let sum2 = countList.reduce((accumulator, currentValue) => {
 }, 0);
 
 console.log(sum3 === sum2);
+
+async function main1() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('hello');
+      resolve('OK');
+    }, 1000);
+  });
+
+  console.log('world');
+}
+
+main1();
+
+function main2() {
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('hello');
+      resolve('OK');
+    }, 2000);
+  }).then((message) => {
+    console.log('world');
+    console.log(message);
+  });
+}
+
+main2();
