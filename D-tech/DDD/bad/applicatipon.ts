1155d446f6ca873880ea95f8e15df2d7564a3ea4
katayama8000 <tattu.0310@gmail.com>
2023-02-20 19:01:37 +0900

    www

---
+++ D-tech/DDD/bad/applicatipon.ts
@@ -0,0 +1,10 @@
+// class TaskApplicationService {
+//   private taskRepository: TaskRepository;
+//   createTask(name: string, dueDate: Date) {
+//     if (name === null || name === undefined) {
+//       throw new Error('You have to configure name and dueDate');
+//     }
+//   }
+
+//   task = new Task();
+// }
