838223b192227cf7b37bc3bc8920e3bbd37c2f3b
katayama8000 <tattu.0310@gmail.com>
2023-07-26 01:37:51 +0900

    add

--- TS/Playground/index3.ts
+++ TS/Playground/index3.ts
@@ -24,3 +24,22 @@ console.log(Sym1 === Sym2); // "false" を返す
 const phantom: unique symbol = Symbol('phantom');
 type Phantom = typeof phantom;
 console.log(phantom);
+
+const objEn = {
+  name: 'bob',
+  language: 'English',
+  age: 20,
+  birthday: new Date(2000, 1, 1),
+};
+
+const objJp = {
+  name: 'bob',
+  language: 'Japanese',
+  age: 20,
+  birthday: new Date(2000, 1, 1),
+};
+
+console.log(Object.entries(objEn));
+for (const [key, value] of Object.entries(objEn)) {
+  console.log(key, value);
+}
