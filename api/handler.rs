6a549506f7d8d005c7f64a866ba1a30d04e69962
katayama800 <tattu.0310@gmail.com>
2024-06-30 08:49:22 +0000

    parse

--- api/handler.rs
+++ api/handler.rs
@@ -1,4 +1,4 @@
-use serde_json::json;
+use serde_json::{json, Value};
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 #[tokio::main]
@@ -8,9 +8,20 @@ async fn main() -> Result<(), Error> {
 
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     let body = req.body();
-    print!("Request body: {:?}", body);
-    println!("Request headers: {:?}", req.headers());
-    println!("Request method: {:?}", req.method());
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+
+    // JSONとしてパース
+    let json_body: Value = serde_json::from_str(&body_str).unwrap();
+
+    println!("body: {}", json_body.to_string());
+
+    // key1とkey2を取り出す
+    let key1 = json_body["body"]["key1"].as_str().unwrap_or("N/A");
+    let key2 = json_body["body"]["key2"].as_str().unwrap_or("N/A");
+
+    println!("key1: {}", key1);
+    println!("key2: {}", key2);
+
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
