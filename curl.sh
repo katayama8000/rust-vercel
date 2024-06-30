83c8ad67971770f96d7fbd0a7c72e258d97d8397
katayama800 <tattu.0310@gmail.com>
2024-06-30 13:49:28 +0000

    update

--- curl.sh
+++ curl.sh
@@ -1,3 +1,5 @@
-curl -X POST https://rust-vercel-gamma.vercel.app/api/handler \
-     -H "Content-Type: application/json" \
-     -d '{"body": {"key1": "value1", "key2": "value2", "array": [1, 2, 3]}, "message": "Hello from Rust!"}'
+curl -X POST https://rust-vercel-gamma.vercel.app/api/handler -H "Content-Type: application/json" -d '{
+  "key1": "value1",
+  "key2": "value2",
+  "key_array": ["item1", "item2", "item3"]
+}'
